d5384dff34e90a54b6e4367bb8113f8f495e4fce
AFoo97 <arthur.foozb@gmail.com>
2025-11-13 11:16:16 +1100

    Finished travel journal

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img 
+        src="./globe.png" 
+        alt="globe" 
+        className="header-img"
+      />
+      <h1 className="header--title">my travel journal.</h1>
+    </header>
+  )
+}
